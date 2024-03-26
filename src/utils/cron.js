0d6da4f0f109848ccb6243bc110f6322b6ab6220
Naresh <nareshbazidpuria.com>
2024-03-26 15:57:54 +0530

    cron added

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -1,5 +1,4 @@
 import cron from "node-cron";
 
 export const keepAlive = () =>
-  //   cron.schedule("*/14 * * * * ", () => console.log(new Date()));
-  cron.schedule("* * * * * * ", () => console.log(new Date()));
+  cron.schedule("*/14 * * * * ", () => console.log(new Date()));
