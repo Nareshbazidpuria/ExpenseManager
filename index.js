f2e9f96b4de5dff8e0a5f32bed20f62365d64e5b
Naresh <nareshbazidpuria.com>
2024-03-26 17:31:06 +0530

    restart cron

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 keepAlive();
 
+app.get("/", (i, o) => o.send("Hello"));
 app.use("/api", apiRouter);
 
 // app.use((err, req, res, next) => {
--- src/utils/cron.js
+++ src/utils/cron.js
@@ -1,4 +1,12 @@
 import cron from "node-cron";
+import https from "https";
 
 export const keepAlive = () =>
-  cron.schedule("*/14 * * * * ", () => console.log(new Date()));
+  cron.schedule("*/14 * * * * ", () => {
+    https
+      .get("https://expensemanager-047k.onrender.com", (res) => {
+        if (res?.statusCode === 200) console.log("server restarted");
+        else console.log("unable to restart the server");
+      })
+      .on("error", (error) => console.log(error));
+  });
