47d02669dfc9a81fe0cc4056df4378dab88c3189
Naresh <nareshbazidpuria@gmail.com>
2024-08-05 18:36:27 +0530

    verification reminder

--- src/api/user/query.js
+++ src/api/user/query.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { User } from "./model";
 
 export const addUserDB = (user) => User.create(user);
@@ -21,6 +22,53 @@ export const getExpoTokensDB = (id) =>
         tokens: {
           $push: "$expoToken",
         },
+      },
+    },
+  ]);
+
+export const notVerifiedEDB = () =>
+  User.aggregate([
+    {
+      $lookup: {
+        from: "groups",
+        localField: "_id",
+        foreignField: "members",
+        as: "groups",
+      },
+    },
+    {
+      $unwind: "$groups",
+    },
+    {
+      $lookup: {
+        from: "expenses",
+        let: { to: "$groups._id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ["$to", { $toString: "$$to" }],
+              },
+              createdAt: { $lte: moment().subtract(1, "day").toDate() },
+              verifiedBy: { $elemMatch: { $ne: "$_id" } },
+            },
+          },
+        ],
+        as: "expenses",
+      },
+    },
+    {
+      $match: {
+        $expr: {
+          $gt: [{ $size: "$expenses" }, 0],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$_id",
+        email: { $first: "$email" },
+        name: { $first: "$name" },
       },
     },
   ]);
--- src/utils/cron.js
+++ src/utils/cron.js
@@ -1,5 +1,8 @@
 import cron from "node-cron";
 import https from "https";
+import { readFileSync } from "fs";
+import { notVerifiedEDB } from "../api/user/query";
+import { sendEmail } from "./mailer";
 
 export const keepAlive = () =>
   cron.schedule("*/14 * * * * ", () => {
@@ -10,3 +13,26 @@ export const keepAlive = () =>
       })
       .on("error", (error) => console.log(error));
   });
+
+export const verifyReminder = () =>
+  cron.schedule(
+    "30 20 * * * ",
+    async () => {
+      try {
+        const users = await notVerifiedEDB(),
+          template = readFileSync(
+            "public/templates/verifyExpenses.html",
+            "utf8"
+          );
+        if (users?.length) {
+          for (const { name, email } of users) {
+            const html = template.replace("{{name}}", name);
+            await sendEmail({ to: email, subject: "Verify Expenses", html });
+          }
+        }
+      } catch (error) {
+        console.log("verify expense cron : ", error);
+      }
+    },
+    { timezone: "Asia/Kolkata" }
+  );
