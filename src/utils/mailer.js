4fac51e075cbba8d4a13bab47ed8c26cc3e7fee4
Naresh Kumar <nareshbazidpuria@gmail.com>
2026-02-28 18:29:33 +0530

    nodemailer gmail API enabled

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,35 +1,85 @@
-import { createTransport } from "nodemailer";
 import { google } from "googleapis";
 
 const user = process.env.MAILER_USER,
   clientId = process.env.GOOGLE_CLIENT_ID,
   clientSecret = process.env.GOOGLE_CLIENT_SECRET,
   refreshToken = process.env.GOOGLE_REFRESH_TOKEN,
-  redirectUri = process.env.GOOGLE_REDIRECT_URI,
-  service = process.env.MAILER_SERVICE;
+  redirectUri = process.env.GOOGLE_REDIRECT_URI;
 
 const OAuth2 = google.auth.OAuth2;
+
 const oauth2Client = new OAuth2(clientId, clientSecret, redirectUri);
 
 oauth2Client.setCredentials({ refresh_token: refreshToken });
 
-export const sendEmail = async (payload) => {
+export const sendEmail = async ({ to, subject, text, html }) => {
   try {
+    if (!to) {
+      throw new Error("Recipient email is required");
+    }
+
     const accessToken = await oauth2Client.getAccessToken();
 
-    const transporter = createTransport({
-      service,
-      auth: { type: "OAuth2", user, clientId, clientSecret, refreshToken, accessToken: accessToken.token },
+    const gmail = google.gmail({ version: "v1", auth: oauth2Client });
+
+    const messageParts = [
+      `From: Expense Manager <${user}>`,
+      `To: ${to}`,
+      `Subject: ${subject}`,
+      "MIME-Version: 1.0",
+      "Content-Type: text/html; charset=UTF-8",
+      "",
+      html || text,
+    ];
+
+    const message = messageParts.join("\n");
+
+    const encodedMessage = Buffer.from(message).toString("base64").replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+
+    const res = await gmail.users.messages.send({
+      userId: "me",
+      requestBody: { raw: encodedMessage },
     });
 
-    const info = await transporter.sendMail({ from: `Expense Manager`, ...payload });
-
-    return { info };
+    return { info: res.data };
   } catch (error) {
-    console.log("mailer error:", error);
+    console.log("GMAIL API ERROR:", error);
     return { error };
   }
 };
+
+// import { createTransport } from "nodemailer";
+// import { google } from "googleapis";
+
+// const user = process.env.MAILER_USER,
+//   clientId = process.env.GOOGLE_CLIENT_ID,
+//   clientSecret = process.env.GOOGLE_CLIENT_SECRET,
+//   refreshToken = process.env.GOOGLE_REFRESH_TOKEN,
+//   redirectUri = process.env.GOOGLE_REDIRECT_URI,
+//   service = process.env.MAILER_SERVICE;
+
+// const OAuth2 = google.auth.OAuth2;
+// const oauth2Client = new OAuth2(clientId, clientSecret, redirectUri);
+
+// oauth2Client.setCredentials({ refresh_token: refreshToken });
+
+// export const sendEmail = async (payload) => {
+//   try {
+//     const accessToken = await oauth2Client.getAccessToken();
+
+//     const transporter = createTransport({
+//       service,
+//       auth: { type: "OAuth2", user, clientId, clientSecret, refreshToken, accessToken: accessToken.token },
+//     });
+
+//     const info = await transporter.sendMail({ from: `Expense Manager`, ...payload });
+
+//     return { info };
+//   } catch (error) {
+//     console.log("mailer error:", error);
+//     return { error };
+//   }
+// };
 
 // import { createTransport } from "nodemailer";
 
